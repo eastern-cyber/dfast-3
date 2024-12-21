b27053c193e7c4733c72b38a8720ebdf073abfeb
katawutp <katawut.paramee@gmail.com.com>
2024-12-21 20:44:14 +0700

    TopNav

---
+++ app/layouts/imcludes/TopNav.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function TopNav() {
+    const router = useRouter()
+    const pathname = usePathname()
+
+    return (
+        <>
+            <div id="TopNav" className="fixed bg-white z-30 flex items-center w-full border-b h-[60px]">
+                <div className={`flex items-center justify-between gap-6 w-full px-4 mx-auto ${pathname === '/' ? 'max-w-[1150px]' : ''}`}>
+                        <a href="/">
+                        <img className="min-w-[115px] w-[115px]" src="/images/3k-logo.png" link="/" />
+                        </a>
+                </div>
+            </div>
+        </>
+    )
+}
