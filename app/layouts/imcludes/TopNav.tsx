b15bb77ec044f5c7df53b6445c0e3ea4d84d40d9
katawutp <katawut.paramee@gmail.com.com>
2024-12-21 20:46:24 +0700

    TopNav LinkFixed

--- app/layouts/imcludes/TopNav.tsx
+++ app/layouts/imcludes/TopNav.tsx
@@ -10,7 +10,7 @@ export default function TopNav() {
             <div id="TopNav" className="fixed bg-white z-30 flex items-center w-full border-b h-[60px]">
                 <div className={`flex items-center justify-between gap-6 w-full px-4 mx-auto ${pathname === '/' ? 'max-w-[1150px]' : ''}`}>
                         <a href="/">
-                        <img className="min-w-[115px] w-[115px]" src="/images/3k-logo.png" link="/" />
+                        <img className="min-w-[115px] w-[115px]" src="/images/3k-logo.png" />
                         </a>
                 </div>
             </div>
