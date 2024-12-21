4e935c433725b8564d3f2bc76256d4a8fc6a6563
katawutp <katawut.paramee@gmail.com.com>
2024-12-21 10:00:40 +0700

    FirstCommit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+//import type { NextConfig } from "next";
+//import next from "next";
+//import { use } from "react";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+      // Add a rule to handle the canvas.node binary module
+      config.module.rules.push({ test: /\.node$/, use: 'raw-loader' });
+  
+      // Exclude canvas from being processed by Next.js in the browser
+      if (!isServer) config.externals.push('canvas');
+      return config;
+  },
+}
 
+// module.exports = nextConfig;
 export default nextConfig;
