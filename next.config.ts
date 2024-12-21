3309f1d6e932ee7384e1fff0547050c9b8119579
katawutp <katawut.paramee@gmail.com.com>
2024-12-21 11:43:53 +0700

    Specify Parameter to Webpack

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,28 @@
 //import next from "next";
 //import { use } from "react";
 
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-      // Add a rule to handle the canvas.node binary module
-      config.module.rules.push({ test: /\.node$/, use: 'raw-loader' });
+// const nextConfig = {
+//   webpack: (config, { isServer }) => {
+//       // Add a rule to handle the canvas.node binary module
+//       config.module.rules.push({ test: /\.node$/, use: 'raw-loader' });
   
-      // Exclude canvas from being processed by Next.js in the browser
-      if (!isServer) config.externals.push('canvas');
-      return config;
-  },
-}
+//       // Exclude canvas from being processed by Next.js in the browser
+//       if (!isServer) config.externals.push('canvas');
+//       return config;
+//   },
+// }
 
 // module.exports = nextConfig;
+// export default nextConfig;
+
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    // Add a rule to handle the canvas.node binary module
+    config.module.rules.push({ test: /\.node$/, use: 'raw-loader' });
+    return config;
+  },
+};
+
 export default nextConfig;
